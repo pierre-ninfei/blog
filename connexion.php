95dcddc28fc48c606378322ccd547eb26db9d9f9
mathieu-tatat <mathieu.tatat@laplateforme.io>
2021-12-08 11:23:23 +0100

    add fichiers

---
+++ connexion.php
@@ -0,0 +1,42 @@
+<?php include('phpspe/inscription&connexion_back.php'); ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Connexion (TEMP)</title>
+	</head>
+
+	<header>
+		<?php //include('header.php'); ?>
+	</header>
+
+	<body>
+		<h1> Connectez-vous </h1>
+
+		<div class="p_c_form">
+			<form method="post" action="connexion.php">
+				<label for="p_loginc"><br/> Nom d'utilisateur </label>
+				<input type="text" name="p_loginc"/>
+
+				<label for="p_passwordc"><br/> Mot de passe </label>
+				<input type="password" name="p_passwordc"/>
+
+				<br/>
+				<input type="submit" name="confirm" value="Connexion"/>
+				<?php if(count($p_errors) > 0) : ?>
+					<div>
+						<?php foreach($p_errors	as $p_error) : ?>
+							<p style="color:red;"> <?php echo $p_error; ?> </p>
+						<?php endforeach ?>
+					</div>
+				<?php endif ?>
+			</form>
+		</div>
+	</body>
+
+	<footer>
+		<?php //include('footer.php'); ?>
+	</footer>
+</html>
---
+++ footer.php
@@ -0,0 +1,50 @@
+   <?php 
+   //_________________display header according to connection status_________________//
+   session_start();
+   echo "
+   <div class='footer_container'>
+      <div class='sitemap'>
+         <a href='index.php'> Accueil </a>
+         <a class='navA' href='articles.php'> Articles </a> ";
+
+   if($_SESSION["id"]==42){
+      echo "<a href='cree-article.php'> Ecrire  </a>";
+   } 
+
+   if(isset($_SESSION["connected"])){
+      if($_SESSION["id"]==1337){
+      echo "<a href='admin.php'> Admin </a>";
+      }
+      else{
+      echo "<a href='profil.php'> Profil </a>";
+      }
+   } 
+   else{
+      echo "<a href='inscription.php'> Inscription </a>";
+   }
+
+   if(isset($_SESSION["connected"])){
+   echo "
+   <form  class ='decoform_footer'method='get'>  
+         <input class='deco_footer'  type='submit' name='deco' value='Se déconnecter'></input>
+         </form>";
+   } 
+   else{
+   echo "<a href='connexion.php'> Connexion </a>";
+   }
+   if(isset($_GET['deco'])){
+   session_destroy();
+   header("Location:index.php");
+   }
+   echo "
+         </div>
+      <div class ='social'>
+      </div>
+   </div>
+   <p id='copyright'>Copyright 2021 - The Blog. - Tous droits réservés<p>
+
+   "
+//    <a class = 'github' href='https://github.com/pierre-ninfei/blog'> Github </a> //
+
+?>
+

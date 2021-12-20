d8c9193a895a7ba8f0b53117ebb3dbf4bc91689e
Antoine Mahérault <antoine.maherault@laplateforme.io>
2021-12-20 09:37:36 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,21 +1,32 @@
    <?php 
    //_________________display header according to connection status_________________//
-
    session_start();
+   $_SESSION["id"]=42;
+   $_SESSION["login"]='n';
+
    echo 
    "<nav><a href='index.php'> Accueil </a>
    <div class='dropdownArticles'>
       <a class='navA' href='articles.php'> Articles <i class='fa fa-angle-down'></i> 
       <div class='dropdown_content'>
-       <a href='categorie1.php'> Projets</a>
-       <a href='categorie2.php'> Veille</a>
-       <a href='categorie3.php'> Design</a>
+       <a href='articles.php?Categorie=Projets'> Projets</a>
+       <a href='articles.php?Categorie=Veille'> Veille</a>
+       <a href='articles.php?Categorie=Design'> Design</a>
       </div>
    </div>
    ";
+   
+   if($_SESSION["id"]==42 || $_SESSION["id"]==1337){
+      echo "<a href='creer-article.php'> Ecrire  </a>";
+   } 
 
    if(isset($_SESSION["login"])){
+      if($_SESSION["id"]==1337){
+      echo "<a href='admin.php'> Admin </a>";
+      }
+      else{
       echo "<a href='profil.php'> Profil </a>";
+      }
    } 
    else{
       echo "<a href='inscription.php'> Inscription </a>";
@@ -23,7 +34,7 @@
 
    if(isset($_SESSION["login"])){
    echo "
-   <form  class ='decoform' method='get'>  
+   <form  class ='decoform'method='get'>  
          <input class='deco'  type='submit' name='deco' value='Se déconnecter'></input>
          </form>";
    } 

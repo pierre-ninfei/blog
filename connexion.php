896c7bef0cd9fb1e0101ab1c4a0ac06ebaa150c8
Pierre-ninfei <pierre.ninfei@laplateforme.io>
2021-12-20 12:24:30 +0100

    fixed issues with overlaping values
    also fixed connexion.php not connecting,
    also fixed inscription.php using session variables for no reasons

--- connexion.php
+++ connexion.php
@@ -1,4 +1,64 @@
-<?php include('phpspe/inscription&connexion_back.php'); ?>
+<?php
+
+session_start();
+
+$p_errors = array();
+
+// connect to bdd
+
+$bdd = mysqli_connect("localhost", "root", "", "blog") or die("impossible de se connecter à la bdd");
+
+//retrieve infos from form
+
+ if(isset($_POST['submit_c'])){
+	if(isset($_POST['p_loginc'])){
+		$p_loginc = mysqli_real_escape_string($bdd, $_POST['p_loginc']);
+	}
+
+	if(isset($_POST['p_passwordc'])){
+		$p_passwordc = mysqli_real_escape_string($bdd, $_POST['p_passwordc']);
+	}
+
+	//Check for form errors
+
+	if(empty($p_loginc)){
+		array_push($p_errors, "Veuillez saisir un nom d'utilisateur");
+	}
+
+	if(empty($p_passwordc)){
+		array_push($p_errors, "Veuillez saisir un mot de passe");
+	}
+
+	//If no errors, attempt log in 
+	
+	if(count($p_errors) == 0 && $p_loginc !=" "){
+		$p_query_c = "SELECT * FROM utilisateurs WHERE (login ='$p_loginc' AND password ='$p_passwordc') OR (email ='$p_loginc' AND password='$p_passwordc')";
+		$p_conn = mysqli_query($bdd, $p_query_c);
+
+		if(mysqli_num_rows($p_conn)){
+			//retrieve user's role ID
+
+			$p_query_id = "SELECT id from utilisateurs WHERE (login='$p_loginc')";
+			$p_id_d_q = mysqli_query($bdd, $p_query_id);
+			$p_id_droits = mysqli_fetch_assoc($p_id_d_q);
+
+			// define new session vars
+
+			$_SESSION['id'] = $p_id_droits;
+			$_SESSION['login'] = $p_loginc;
+			$_SESSION['success'] = "Connexion validée";
+
+			header('location: index.php');
+		}
+
+		//If no account is found in the bdd
+		
+		else{
+			array_push($p_errors, "Identifiant ou mot de passe invalide");
+		}
+	}
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -21,26 +81,28 @@
 		<h1 class="sm_title"><i>The BLOG.  </i></h1> 
 		<h1 class="p_i_intro_txt" > Connectez-vous </h1>
 		<div class="p_i_container"> 
-		<div class="p_i_form">
-			<form method="post" action="connexion.php">
-				<label for="p_loginc"><br/> Nom d'utilisateur </label>
-				<input type="text" name="p_loginc"/>
+			<div class="p_i_form">
+				<form method="post" action="connexion.php">
+					<label for="p_loginc"><br/> Nom d'utilisateur </label>
+					<input type="text" name="p_loginc"/>
 
-				<label for="p_passwordc"><br/> Mot de passe </label>
-				<input type="password" name="p_passwordc"/>
+					<label for="p_passwordc"><br/> Mot de passe </label>
+					<input type="password" name="p_passwordc"/>
 
-				<br/>
-				<input type="submit" name="confirm" value="Connexion"/>
-				<?php if(count($p_errors) > 0) : ?>
-					<div>
-						<?php foreach($p_errors	as $p_error) : ?>
-							<p style="color:red;"> <?php echo $p_error; ?> </p>
-						<?php endforeach ?>
-					</div>
-				<?php endif ?>
-			</form>
-		</div>
+					<br/>
+					<input type="submit" name="submit_c" value="Valider"/>
+					<?php // counts errors and return values of each
+
+					if(count($p_errors) > 0) : ?>
+						<div>
+							<?php foreach($p_errors	as $p_error) : ?>
+								<p style="color:red;"> <?php echo $p_error; ?> </p>
+							<?php endforeach ?>
 						</div>
+					<?php endif ?>
+				</form>
+			</div>
+		</div>
 	</body>
 	</main>
 	<footer>
--- inscription.php
+++ inscription.php
@@ -1,4 +1,80 @@
-<?php include('phpspe/inscription&connexion_back.php'); ?>
+<?php 
+
+$p_errors = array();
+
+//connect to bdd 
+
+$bdd = mysqli_connect("localhost", "root", "", "blog") or die("impossible de se connecter à la bdd");
+
+//retrieve infos from form
+
+if(isset($_POST['submit_i'])){
+	if(isset($_POST['p_login'])){
+		$p_login = mysqli_real_escape_string($bdd, $_POST['p_login']);
+	}
+
+	if(isset($_POST['p_email'])){
+		$p_email = mysqli_real_escape_string($bdd, $_POST['p_email']);
+	}
+
+	if(isset($_POST['p_passw'])){
+		$p_passw1 = mysqli_real_escape_string($bdd, $_POST['p_passw']);
+	}
+
+	if(isset($_POST['p_passwc'])){
+		$p_passw2 = mysqli_real_escape_string($bdd, $_POST['p_passwc']);
+	}
+
+
+	//Check for form errors
+
+	if(empty($p_login)){
+		array_push($p_errors, "Veuillez saisir un nom d'utilisateur");
+	}
+
+	if(empty($p_email) || !filter_var($p_email, FILTER_VALIDATE_EMAIL)){
+		array_push($p_errors, "Veuillez saisir une adresse mail valide");
+	}	
+
+	if(empty($p_passw1)){
+		array_push($p_errors, "Veuillez saisir un mot de passe");
+	}
+
+	if($p_passw2 != $p_passw1){
+		array_push($p_errors, "Veuillez saisir un mot de passe identique");
+	}
+
+	// Check if login and email already are in use in the bdd
+
+	$p_user_check = "SELECT * FROM utilisateurs WHERE login ='$p_login' LIMIT 1";
+	$p_query = mysqli_query($bdd, $p_user_check);
+	$p_user = mysqli_fetch_assoc($p_query);
+	if($p_user){
+		if($p_user['login'] === $p_login){
+			array_push($p_errors, "Ce nom d'utilisateur est déjà utilisé !");
+		}
+	}
+
+	$p_email_check = "SELECT * FROM utilisateurs WHERE email = '$p_email' LIMIT 1";
+	$p_query2 = mysqli_query($bdd, $p_email_check);
+	$p_mail = mysqli_fetch_assoc($p_query2);
+	if($p_mail){
+		if($p_mail['email'] == $p_email){
+			array_push($p_errors, "Cette adresse mail est déjà utilisée !"); 
+		}
+	}
+
+	//if there are no errors, register user's account
+
+	if(count($p_errors) == 0 && $p_login != " "){
+		$p_password = $p_passw1;
+		$p_queryr = "INSERT INTO utilisateurs(login, password, email, id_droits) VALUES ('$p_login', '$p_password', '$p_email', 1)";
+		mysqli_query($bdd, $p_queryr);
+
+		header('location: connexion.php');
+	}
+}
+?>
 
 <!DOCTYPE html>
 <html>
@@ -36,7 +112,7 @@
 					<input type="password" name="p_passwc"/>
 
 					<br/>
-					<input type="submit" value="Valider"/>
+					<input type="submit" name ="submit_i" value="Valider"/>
 
 					<?php // counts errors and return values of each
 

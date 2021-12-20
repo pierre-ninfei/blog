f830a007cac1078bfa3ec917306fc04e345ffa61
Pierre-ninfei <pierre.ninfei@laplateforme.io>
2021-12-20 14:12:17 +0100

    moved php blocks, changed page titles

--- connexion.php
+++ connexion.php
@@ -1,81 +1,81 @@
-<?php
-
-session_start();
-
-$p_errors = array();
-
-// connect to bdd
-
-$bdd = mysqli_connect("localhost", "root", "", "blog") or die("impossible de se connecter à la bdd");
-
-//retrieve infos from form
-
- if(isset($_POST['submit_c'])){
-	if(isset($_POST['p_loginc'])){
-		$p_loginc = mysqli_real_escape_string($bdd, $_POST['p_loginc']);
-	}
-
-	if(isset($_POST['p_passwordc'])){
-		$p_passwordc = mysqli_real_escape_string($bdd, $_POST['p_passwordc']);
-	}
-
-	//Check for form errors
-
-	if(empty($p_loginc)){
-		array_push($p_errors, "Veuillez saisir un nom d'utilisateur");
-	}
-
-	if(empty($p_passwordc)){
-		array_push($p_errors, "Veuillez saisir un mot de passe");
-	}
-
-	//If no errors, attempt log in 
-	
-	if(count($p_errors) == 0 && $p_loginc !=" "){
-		$p_query_c = "SELECT password FROM utilisateurs WHERE login ='$p_loginc'";
-		$p_conn = mysqli_query($bdd, $p_query_c);
-		$p_password_q = mysqli_fetch_array($p_conn);
-
-		if(mysqli_num_rows($p_conn)){
-
-			if(password_verify($_POST['p_passwordc'], $p_password_q[0]) == true){
-
-				//retrieve user's role ID
-
-				$p_query_id = "SELECT id from utilisateurs WHERE (login='$p_loginc')";
-				$p_id_d_q = mysqli_query($bdd, $p_query_id);
-				$p_id_droits = mysqli_fetch_assoc($p_id_d_q);
-
-				// define new session vars
-
-				$_SESSION['id'] = $p_id_droits;
-				$_SESSION['login'] = $p_loginc;
-				$_SESSION['success'] = "Connexion validée";
-
-				header('location: index.php');
-			}
-
-			//If no account is found in the bdd
-			
-			else{
-				array_push($p_errors, "Identifiant ou mot de passe invalide");
-			}
-		}
-	}
-}
-?>
-
 <!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" href="style.css">
-		<title>Connexion (TEMP)</title>
+		<title>Connectez vous au BLOG.</title>
 		<meta charset="utf-8">
   		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
  		<link rel="stylesheet" href="style.css">
 	</head>
+
+	<?php
+
+	session_start();
+
+	$p_errors = array();
+
+	// connect to bdd
+
+	$bdd = mysqli_connect("localhost", "root", "", "blog") or die("impossible de se connecter à la bdd");
+
+	//retrieve infos from form
+
+	 if(isset($_POST['submit_c'])){
+		if(isset($_POST['p_loginc'])){
+			$p_loginc = mysqli_real_escape_string($bdd, $_POST['p_loginc']);
+		}
+
+		if(isset($_POST['p_passwordc'])){
+			$p_passwordc = mysqli_real_escape_string($bdd, $_POST['p_passwordc']);
+		}
+
+		//Check for form errors
+
+		if(empty($p_loginc)){
+			array_push($p_errors, "Veuillez saisir un nom d'utilisateur");
+		}
+
+		if(empty($p_passwordc)){
+			array_push($p_errors, "Veuillez saisir un mot de passe");
+		}
+
+		//If no errors, attempt log in 
+		
+		if(count($p_errors) == 0 && $p_loginc !=" "){
+			$p_query_c = "SELECT password FROM utilisateurs WHERE login ='$p_loginc'";
+			$p_conn = mysqli_query($bdd, $p_query_c);
+			$p_password_q = mysqli_fetch_array($p_conn);
+
+			if(mysqli_num_rows($p_conn)){
+
+				if(password_verify($_POST['p_passwordc'], $p_password_q[0]) == true){
+
+					//retrieve user's role ID
+
+					$p_query_id = "SELECT id from utilisateurs WHERE (login='$p_loginc')";
+					$p_id_d_q = mysqli_query($bdd, $p_query_id);
+					$p_id_droits = mysqli_fetch_assoc($p_id_d_q);
+
+					// define new session vars
+
+					$_SESSION['id'] = $p_id_droits;
+					$_SESSION['login'] = $p_loginc;
+					$_SESSION['success'] = "Connexion validée";
+
+					header('location: index.php');
+				}
+
+				//If no account is found in the bdd
+				
+				else{
+					array_push($p_errors, "Identifiant ou mot de passe invalide");
+				}
+			}
+		}
+	}
+	?>
 
 	<header>
 		<?php include('header.php'); ?>
--- inscription.php
+++ inscription.php
@@ -1,82 +1,3 @@
-<?php 
-
-$p_errors = array();
-
-//connect to bdd 
-
-$bdd = mysqli_connect("localhost", "root", "", "blog") or die("impossible de se connecter à la bdd");
-
-//retrieve infos from form
-
-if(isset($_POST['submit_i'])){
-	if(isset($_POST['p_login'])){
-		$p_login = mysqli_real_escape_string($bdd, $_POST['p_login']);
-	}
-
-	if(isset($_POST['p_email'])){
-		$p_email = mysqli_real_escape_string($bdd, $_POST['p_email']);
-	}
-
-	if(isset($_POST['p_passw'])){
-		$p_passw1 = mysqli_real_escape_string($bdd, $_POST['p_passw']);
-	}
-
-	if(isset($_POST['p_passwc'])){
-		$p_passw2 = mysqli_real_escape_string($bdd, $_POST['p_passwc']);
-	}
-
-
-	//Check for form errors
-
-	if(empty($p_login)){
-		array_push($p_errors, "Veuillez saisir un nom d'utilisateur");
-	}
-
-	if(empty($p_email) || !filter_var($p_email, FILTER_VALIDATE_EMAIL)){
-		array_push($p_errors, "Veuillez saisir une adresse mail valide");
-	}	
-
-	if(empty($p_passw1)){
-		array_push($p_errors, "Veuillez saisir un mot de passe");
-	}
-
-	if($p_passw2 != $p_passw1){
-		array_push($p_errors, "Veuillez saisir un mot de passe identique");
-	}
-
-	// Check if login and email already are in use in the bdd
-
-	$p_user_check = "SELECT * FROM utilisateurs WHERE login ='$p_login' LIMIT 1";
-	$p_query = mysqli_query($bdd, $p_user_check);
-	$p_user = mysqli_fetch_assoc($p_query);
-	if($p_user){
-		if($p_user['login'] === $p_login){
-			array_push($p_errors, "Ce nom d'utilisateur est déjà utilisé !");
-		}
-	}
-
-	$p_email_check = "SELECT * FROM utilisateurs WHERE email = '$p_email' LIMIT 1";
-	$p_query2 = mysqli_query($bdd, $p_email_check);
-	$p_mail = mysqli_fetch_assoc($p_query2);
-	if($p_mail){
-		if($p_mail['email'] == $p_email){
-			array_push($p_errors, "Cette adresse mail est déjà utilisée !"); 
-		}
-	}
-
-	//if there are no errors, register user's account
-
-	//starting with password hashing
-	if(count($p_errors) == 0 && $p_login != " "){
-		$p_password = password_hash($p_passw1, PASSWORD_DEFAULT);
-		$p_queryr = "INSERT INTO utilisateurs(login, password, email, id_droits) VALUES ('$p_login', '$p_password', '$p_email', 1)";
-		mysqli_query($bdd, $p_queryr);
-
-		header('location: connexion.php');
-	}
-}
-?>
-
 <!DOCTYPE html>
 <html>
 	<head>
@@ -84,10 +5,88 @@ if(isset($_POST['submit_i'])){
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<link rel="stylesheet" type="text/css" href="inscription.css">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-		<title>The BLOG</title>
+		<title>Inscrivez vous au BLOG</title>
 		<link rel="stylesheet" href="style.css">
-
 	</head>
+
+	<?php 
+
+	$p_errors = array();
+
+	//connect to bdd 
+
+	$bdd = mysqli_connect("localhost", "root", "", "blog") or die("impossible de se connecter à la bdd");
+
+	//retrieve infos from form
+
+	if(isset($_POST['submit_i'])){
+		if(isset($_POST['p_login'])){
+			$p_login = mysqli_real_escape_string($bdd, $_POST['p_login']);
+		}
+
+		if(isset($_POST['p_email'])){
+			$p_email = mysqli_real_escape_string($bdd, $_POST['p_email']);
+		}
+
+		if(isset($_POST['p_passw'])){
+			$p_passw1 = mysqli_real_escape_string($bdd, $_POST['p_passw']);
+		}
+
+		if(isset($_POST['p_passwc'])){
+			$p_passw2 = mysqli_real_escape_string($bdd, $_POST['p_passwc']);
+		}
+
+
+		//Check for form errors
+
+		if(empty($p_login)){
+			array_push($p_errors, "Veuillez saisir un nom d'utilisateur");
+		}
+
+		if(empty($p_email) || !filter_var($p_email, FILTER_VALIDATE_EMAIL)){
+			array_push($p_errors, "Veuillez saisir une adresse mail valide");
+		}	
+
+		if(empty($p_passw1)){
+			array_push($p_errors, "Veuillez saisir un mot de passe");
+		}
+
+		if($p_passw2 != $p_passw1){
+			array_push($p_errors, "Veuillez saisir un mot de passe identique");
+		}
+
+		// Check if login and email already are in use in the bdd
+
+		$p_user_check = "SELECT * FROM utilisateurs WHERE login ='$p_login' LIMIT 1";
+		$p_query = mysqli_query($bdd, $p_user_check);
+		$p_user = mysqli_fetch_assoc($p_query);
+		if($p_user){
+			if($p_user['login'] === $p_login){
+				array_push($p_errors, "Ce nom d'utilisateur est déjà utilisé !");
+			}
+		}
+
+		$p_email_check = "SELECT * FROM utilisateurs WHERE email = '$p_email' LIMIT 1";
+		$p_query2 = mysqli_query($bdd, $p_email_check);
+		$p_mail = mysqli_fetch_assoc($p_query2);
+		if($p_mail){
+			if($p_mail['email'] == $p_email){
+				array_push($p_errors, "Cette adresse mail est déjà utilisée !"); 
+			}
+		}
+
+		//if there are no errors, register user's account
+
+		//starting with password hashing
+		if(count($p_errors) == 0 && $p_login != " "){
+			$p_password = password_hash($p_passw1, PASSWORD_DEFAULT);
+			$p_queryr = "INSERT INTO utilisateurs(login, password, email, id_droits) VALUES ('$p_login', '$p_password', '$p_email', 1)";
+			mysqli_query($bdd, $p_queryr);
+
+			header('location: connexion.php');
+		}
+	}
+	?>
 
 	<header>
     	<?php include "header.php";?> 

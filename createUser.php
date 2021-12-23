944e43aca1dc7aa2283a6b425cc0e951f0f29938
Pierre-ninfei <pierre.ninfei@laplateforme.io>
2021-12-23 10:58:39 +0100

    added password hash

--- createUser.php
+++ createUser.php
@@ -65,7 +65,8 @@
                     ///////////// requete pour envoyer les infos dans la bdd\\\\\\\\\\\ -->
 
                                 if($login == $confname && $password == $confpassword && $email == $confemail && $taken == false){
-                                    $req = mysqli_query($conn,"INSERT INTO utilisateurs(login, password, email, id_droits) VALUES ('$login','$password','$email','$id_droits')");
+                                     $npassword = password_hash($password, PASSWORD_DEFAULT);
+                                    $req = mysqli_query($conn,"INSERT INTO utilisateurs(login, password, email, id_droits) VALUES ('$login','$npassword','$email','$id_droits')");
                                     header('Location: admin.php');
                                     
                             }               
